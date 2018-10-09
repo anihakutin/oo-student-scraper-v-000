ba3a00843b095d69806c8e2f6197dbc4bdcdcaad
Heshie Brody <4hbrody@gmail.com>
2018-10-09 01:42:38 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,9 +15,8 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |key, value|
-
-    end
+    attributes_hash.each {|k, v| send(:"#{k}=", v)}
+    self
   end
 
   def self.all
